30c09a9123cb508ea7145026665670c7fe9dab88
Ousamne CAMARA <122101496+ouzecamara90@users.noreply.github.com>
2023-05-17 10:40:02 +0000

    second commit

--- src/userList.js
+++ src/userList.js
@@ -1,31 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {useEffect} from 'react';
-import {useState} from 'react';
-import listOfUsers from 'react';
 
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
 
-const [listOfUsers, setListOfUsers] = useState([]);
-
-
-
-useEffect(() => {
-    axios.get('https://jasonplaceholder.typicode.com/users').then(response =>{
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
         setListOfUsers(response.data);
-    }).catch(error => {
-        console.error('Error fetching user data:',error);
-    });
-},[]);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données utilisateur :', error);
+      });
+  }, []);
 
-
-
-return (
+  return (
     <div>
       <h1>Liste des utilisateurs :</h1>
       <ul>
-        {listOfUsers.map(user => (
+        {listOfUsers.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </div>
   );
-  
+};
+
+export default UserList;
